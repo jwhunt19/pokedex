55fcb1dece3fb7101b26d7d02d972a147f65f942
Joey Huntington <jwhunt19@gmail.com>
2024-02-15 18:51:44 -0700

    barebones pokedex preview

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as pokeAPI from "./services/pokeApi";
 
 function App() {
   const [pokemon, setPokemon] = useState({});
   const [input, setInput] = useState("");
 
+  useEffect(() => {
+    if (!pokemon.id) {
+      pokeAPI.getPokemon("pikachu").then((data) => setPokemon(data));
+    }
+  });
+
   const handleInput = (e) => {
     setInput(e.target.value);
-  }
+  };
 
   const search = () => {
-    pokeAPI.getPokemon(input).then((data) => setPokemon(data))
+    pokeAPI.getPokemon(input).then((data) => setPokemon(data));
   };
 
   return (
@@ -22,7 +28,18 @@ function App() {
         onChange={handleInput}
       ></input>
       <button onClick={search}>search</button>
-      {pokemon.name ? <p>{pokemon.name}</p> : ''}
+      {pokemon.name ? (
+        <>
+          <p>{`id: ${pokemon.id}\n`}</p>
+          <p>{`name: ${pokemon.name}`}</p>
+          <img src={pokemon.sprite} alt={`sprite for ${pokemon.name}`} />
+          <p>{`types: ${pokemon.types.join(", ")}`}</p>
+          <p>{`stats: ${JSON.stringify(pokemon.stats)}`}</p>
+          <p>{`abilities: ${pokemon.abilities.join(", ")}`}</p>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
--- src/utils/pokeApiUtils.js
+++ src/utils/pokeApiUtils.js
@@ -2,17 +2,17 @@ const shapeData = (data) => {
   const shapedData = {
     id: data.id,
     name: data.name,
-    image: data.sprites.front_default,
+    sprite: data.sprites.front_default,
     types: data.types.map((type) => type.type.name),
     stats: {},
-    abilities: data.abilities.map((ability) => ability.ability.name)
+    abilities: data.abilities.map((ability) => ability.ability.name),
   };
-  
+
   data.stats.forEach((stat) => {
     shapedData.stats[stat.stat.name] = stat.base_stat;
   });
 
   return shapedData;
-}
+};
 
-export { shapeData }
+export { shapeData };
